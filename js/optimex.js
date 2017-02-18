5c8aa05decfa230e73867a897c7906818e7fa86a
jeeve <jvjulien@free.fr>
2017-02-19 00:51:29 +0100

    vues

--- js/optimex.js
+++ js/optimex.js
@@ -1,22 +1,22 @@
-var app = angular.module('opt', ['ngRoute']);
+var app = angular.module('optimex', ['ngRoute']);
 	
-app.controller('optima', ['$scope', '$http', function($scope, $http) {
+app.controller('optimexCtrl', ['$scope', '$http', function($scope, $http) {
  
 	$http.get('rest/api.php/alliage').success(function(response) {
 		$scope.alliages = php_crud_api_transform(response).alliage;	
 	});
 
 	var minimaxis = null;
-	$http.get('rest/api.php/minimaxi').success(function(response) {
-		minimaxis = php_crud_api_transform(response).minimaxi; 
+	$http.get('rest/api.php/v_minimaxi').success(function(response) {
+		minimaxis = php_crud_api_transform(response).v_minimaxi; 
 	});		
 
 	$http.get('rest/api.php/matiere').success(function(response) {
 		$scope.matieres = php_crud_api_transform(response).matiere; 
 		
 		var compos = null;
-		$http.get('rest/api.php/compo').success(function(response) {
-			compos = php_crud_api_transform(response).compo; 
+		$http.get('rest/api.php/v_compo').success(function(response) {
+			compos = php_crud_api_transform(response).v_compo; 
 			for (var i = 0; i < $scope.matieres.length; i++) {
 				for (var j = 0; j < compos.length; j++) {
 					if ($scope.matieres[i].CodeMatiere == compos[j].CodeMatiere) {
