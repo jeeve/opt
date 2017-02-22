8657c0b0db425459ed6cfb62f6edc61e3949d161
jeeve <jvjulien@free.fr>
2017-02-22 22:48:05 +0100

    bouton enfonce

--- app/directives/dir-table-alliage.js
+++ app/directives/dir-table-alliage.js
@@ -5,9 +5,14 @@ app.directive("dirTableAlliage", function() {
 		scope: { alliage: '=table'},
         templateUrl: 'app/directives/dir-table-alliage.html',
 		link: function(scope, element, attrs){
+			scope.etatBouton = 'btn-releve';
             scope.showMe = false;
             scope.toggle = function toggle(){
                 scope.showMe = !scope.showMe;
+				if (scope.etatBouton == 'btn-releve')
+					scope.etatBouton = 'btn-enfonce'
+				else
+					scope.etatBouton = 'btn-releve';
             };
         }
         }
--- app/directives/dir-table-matiere.js
+++ app/directives/dir-table-matiere.js
@@ -4,10 +4,15 @@ app.directive("dirTableMatiere", function() {
         replace: false,
 		scope: { matiere: '=table'},
         templateUrl: 'app/directives/dir-table-matiere.html',
-		link: function(scope, element, attrs){
+		link: function(scope, element, attrs) {
+			scope.etatBouton = 'btn-releve';
             scope.showMe = false;
             scope.toggle = function toggle(){
                 scope.showMe = !scope.showMe;
+				if (scope.etatBouton == 'btn-releve')
+					scope.etatBouton = 'btn-enfonce'
+				else
+					scope.etatBouton = 'btn-releve';
             };
 		}
         }
