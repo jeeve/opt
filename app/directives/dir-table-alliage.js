2ed68f8e4ef704deecc78dc55ef6260ff38cae22
jeeve <jvjulien@free.fr>
2017-02-22 17:15:55 +0100

    edit

--- app/directives/dir-table-alliage.js
+++ app/directives/dir-table-alliage.js
@@ -3,6 +3,12 @@ app.directive("dirTableAlliage", function() {
         restrict: 'E',
         replace: false,
 		scope: { alliage: '=table'},
-        templateUrl: 'app/directives/dir-table-alliage.html'
+        templateUrl: 'app/directives/dir-table-alliage.html',
+		link: function(scope, element, attrs){
+            scope.showMe = false;
+            scope.toggle = function toggle(){
+                scope.showMe = !scope.showMe;
+            };
+        }
         }
 });
--- app/directives/dir-table-matiere.js
+++ app/directives/dir-table-matiere.js
@@ -3,6 +3,12 @@ app.directive("dirTableMatiere", function() {
         restrict: 'E',
         replace: false,
 		scope: { matiere: '=table'},
-        templateUrl: 'app/directives/dir-table-matiere.html'
+        templateUrl: 'app/directives/dir-table-matiere.html',
+		link: function(scope, element, attrs){
+            scope.showMe = false;
+            scope.toggle = function toggle(){
+                scope.showMe = !scope.showMe;
+            };
+		}
         }
 });
