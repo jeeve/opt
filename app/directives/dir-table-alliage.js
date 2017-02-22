726657b868af7f9a94d2b9f662266141636a35c4
jeeve <jvjulien@free.fr>
2017-02-22 14:56:44 +0100

    table=

--- app/directives/dir-table-alliage.js
+++ app/directives/dir-table-alliage.js
@@ -2,6 +2,7 @@ app.directive("dirTableAlliage", function() {
     return {
         restrict: 'E',
         replace: false,
+		scope: { alliage: '=table'},
         templateUrl: 'app/directives/dir-table-alliage.html'
         }
 });
